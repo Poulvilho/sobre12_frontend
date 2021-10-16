3b011ae1a6fba4cdcac146317edf7e85746c9cc1
Poulvilho <pv.paulovictor.vp@gmail.com>
2021-10-16 20:24:54 -0300

    Add comunication

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
@@ -6,9 +6,20 @@ import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const [itens, setItens] = useState<string>('');
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users/login")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setItens(result.message);
+  })}, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Tábua Um</Text>
+      <Text>{itens}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
