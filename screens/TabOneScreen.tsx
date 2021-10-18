1c28ba48aa6008bf54fe8bb61e9ecd37ac1399c8
Poulvilho <pv.paulovictor.vp@gmail.com>
2021-10-18 16:36:44 -0300

    Add axios

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,3 +1,5 @@
+import { isTemplateElement } from '@babel/types';
+import axios, { AxiosResponse } from 'axios';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 
@@ -6,20 +8,27 @@ import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  const [itens, setItens] = useState<string>('');
+
+  interface user {
+    email: string;
+    password: string;
+  };
+  
+  const [item, setItem] = useState<user>({email: '', password: ''});
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/users/login")
-    .then(res => res.json())
-    .then(
-      (result) => {
-        setItens(result.message);
-  })}, []);
+    axios.post<user>("http://localhost:3000/api/users/login", { email: "hello@world.com", password: 'password' })
+    .then(response  => {
+      console.log(response);
+      return setItem(response.data);
+    })
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tábua Um</Text>
-      <Text>{itens}</Text>
+      <Text style={styles.title}>Tab One</Text>
+      <Text>Email: {item.email}</Text>
+      <Text>Senha: {item.password}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
