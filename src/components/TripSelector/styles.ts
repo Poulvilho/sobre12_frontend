833247fe8b8d8b9d60da69c042b919dda03cf58c
jpmota2208 <jpmota.unb@gmail.com>
2022-06-24 23:07:47 -0300

    Start TripSelector component

---
+++ src/components/TripSelector/styles.ts
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#E5E5E5',
+    width: '95%',
+    height: 100,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 5,
+  },
+  PrimaryText:{
+    fontSize: 16,
+    fontWeight: '700',
+    color: '#000000',
+    textAlign: 'center',
+  },
+  content: {
+    flexDirection: 'row',
+    alignContent: 'center',
+  },
+  image:{
+    margin: 10,
+    width: 80,
+    height: 80,
+    backgroundColor: '#42A4E8',
+  },
+  data:{
+    
+  },
+})
